e44a6443cfdeadf28d53f049ad7464a82c0ec933
striaya <shenggaryoselino@gmail.com>
2026-01-19 15:01:28 +0700

    LaravelBuLia

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Larawell</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  </head>
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div>
+                    <h3 class text ='text-center my-4'>List Products</h3>
+                </div>
+                <div class="card border=0 shadow-sm rounded">
+                    <div class="card-body">
+                        <a href="" class="btn btn-md btn-success mb-3">Add Product</a>
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Image</th>
+                                    <th scope="col">Title</th>
+                                    <th scope="col">Price</th>
+                                    <th scope="col">Stock</th>
+                                    <th scope="col" style="width: 20%">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($products as $product)
+                                <tr>
+                                    <td class="text-center">
+                                        <img src="{{ asset('/storage/products/' . $product->image) }}" class="rounded" style="width: 150px" alt="">
+                                    </td>
+                                    <td>{{ $product->title }}</td>
+                                    <td>{{ "Rp" . number_format($product->price, 2, ',','.') }}</td>
+                                    <td>{{ $product->stock }}</td>
+                                    <td class="text-center">
+                                        <form onsubmit="return confirm('Apakah anda yakin?');" action="" method="POST">
+                                            <a href="" class="btn btn-sm btn dark">Show</a>
+                                            <a href="" class="btn btn-sm btn primary">Edit</a>
+                                            @csrf
+                                            @methode DELETE
+                                            <button type="submit" class="btn btn-sm btn-danger">Hapus</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                                @empty
+                                <div class="alert alert-danger">
+                                    Data Product belum tersedia
+                                </div>
+                                @endforelse
+                            </tbody>
+                        </table>
+                        {{ $products->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</body>
+</html>
