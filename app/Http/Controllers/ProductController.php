04c372afb0a670b599a909fa998f4de1382ebb64
striaya <shenggaryoselino@gmail.com>
2026-01-20 10:37:06 +0700

    Update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -10,6 +11,35 @@ class ProductController extends Controller
     public function index()
     {
         $products = Product::latest()->paginate(10);
+
         return view('products.index', compact('products'));
+    }
+
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'image' => 'required|image|mimes:jpeg,jpg,png|max:2048',
+            'title' => 'required|string|max:255|min:3',
+            'description' => 'required|string|min:10',
+            'price' => 'required|numeric',
+            'stock' => 'required|integer',
+        ]);
+        $image = $request->file('image');
+        $imagePath = $image->storeAs('products', $image->hashName());
+
+        Product::create([
+            'image' => $imagePath,
+            'title' => $request->title,
+            'description' => $request->description,
+            'price' => $request->price,
+            'stock' => $request->stock,
+        ]);
+
+        return redirect()->route('products.index')->with('success', 'Product created successfully.');
     }
 }
